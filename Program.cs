54e88e8d4bee545b5b133868102bead41d9b8194
Intermetheus <93767470+Intermetheus@users.noreply.github.com>
2022-02-04 12:51:35 +0100

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace StrategyRTS
+{
+    public static class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+            using (var game = new GameWorld())
+                game.Run();
+        }
+    }
+}
