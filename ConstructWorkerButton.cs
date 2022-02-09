75386759dd5d3e8be7725940a6f72dd3cac6cd32
Intermetheus <93767470+Intermetheus@users.noreply.github.com>
2022-02-09 13:16:23 +0100

    Update ConstructWorkerButton.cs

--- ConstructWorkerButton.cs
+++ ConstructWorkerButton.cs
@@ -12,13 +12,14 @@ namespace StrategyRTS
     {
         private Texture2D sprite;
         private Rectangle bounds;
+        private int gasWorkerCost = 1;
         public bool isHovering;
-        public static Mutex drawMutex = new Mutex();
+        public static Mutex drawMutex = new Mutex();        
 
         public ConstructWorkerButton()
         {
             //sprite = content.Load<Texture2D>("Build_Button");
-            position = new Vector2(1600 / 2 - 300, 900 - 50);
+            position = new Vector2(1600 / 2 - 400, 900 - 150);
             //base.spriteBatch = spriteBatch;
             //UIThread = new Thread(ThreadMethod);
             //UIThread.IsBackground = true;
@@ -34,13 +35,14 @@ namespace StrategyRTS
         public override void Draw(SpriteBatch spriteBatch)
         {
             spriteBatch.Draw(sprite, position, null, Color.White, 0, Vector2.Zero, 1, SpriteEffects.None, 0);
+            spriteBatch.DrawString(GameWorld.Arial, $"New Gas Worker: {gasWorkerCost} Mineral", position + new Vector2(-100, 50), Color.Black, 0f, Vector2.Zero, 1f, SpriteEffects.None, 1f); ;
         }
 
         public override void Update(GameTime gameTime)
         {
-            if (Base.MineralAmount >= 5 && bounds.Contains(GameWorld.MouseStateProp.Position) && GameWorld.LeftMouseButtonReleased())
+            if (Base.MineralAmount >= gasWorkerCost && bounds.Contains(GameWorld.MouseStateProp.Position) && GameWorld.LeftMouseButtonReleased())
             {
-                GameWorld.MyBase.RemoveMinerals(1);
+                GameWorld.MyBase.RemoveMinerals(gasWorkerCost);
                 GameWorld.NewGameObjects.Add(new TimedGasWorker());
             }
         }
