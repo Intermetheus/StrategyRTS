71ec8bddbe3477f0ab29b86111f13cf8cf1caf72
Intermetheus <93767470+Intermetheus@users.noreply.github.com>
2022-02-11 10:12:52 +0100

    Update GameWorld.cs

--- GameWorld.cs
+++ GameWorld.cs
@@ -5,6 +5,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading;
 
+//Gruppe 1: Casper Lykke, Jonathan Saugbjerg, Tobias Krogshede, Oscar Halbe
+
 namespace StrategyRTS
 {
     public class GameWorld : Game
